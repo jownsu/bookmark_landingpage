55b64c21211545288863ed1b5bfc9fc4e6a28ed0
Jownsu <jhonesdigno666@gmail.com>
2024-04-13 21:40:58 +0800

    Implement hero and features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
     content: ["./*.html"],
     theme: {
-        extend: {}
+        fontFamily: {
+            sans: ["Rubik", "ui-sans-serif"]
+        },
+        container: {
+            center: true,
+            screens: {
+                lg: "110.4em"
+            },
+            padding: {
+                DEFAULT: "3.2rem",
+                lg: 0
+            }
+        },
+        extend: {
+            colors: {
+                primary: "#5267DF",
+                secondary: "#FA5959",
+                dark: "#242A45",
+                grey: "#9194A2",
+                light_grey: "#F7F7F7"
+            }
+        }
     },
     plugins: []
 };
