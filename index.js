e06f84e1bc1910ce2b41314d18fdb1b8b0fab7a4
Jownsu <jhonesdigno666@gmail.com>
2024-04-27 17:22:21 +0800

    Features tab function

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const nav_trigger_close = document.querySelector("#nav_trigger_close");
     const mobile_nav = document.querySelector("#mobile_nav");
     const mobile_nav_links = document.querySelectorAll("#mobile_nav a");
+    const feature_tabs_btns = document.querySelectorAll("#features_tab button");
+    const features = document.querySelectorAll("#features_container > div");
 
     accordion_trigger_btns.forEach(accordion_trigger => {
         accordion_trigger.addEventListener("click", (event) => {
             toggleAccordionContent(event.target);
+        });
+    });
+
+    feature_tabs_btns.forEach(tab_btn => {
+        tab_btn.addEventListener("click", () => {
+            toggleFeatureContent(tab_btn)
         });
     });
 
@@ -25,6 +33,28 @@ document.addEventListener("DOMContentLoaded", () => {
             closeMobileNav();
         })
     });
+
+    const toggleFeatureContent = (tab_btn) => {
+        const trigger_id = tab_btn.getAttribute("data-feature-id");
+
+        feature_tabs_btns.forEach(tabBtn => {
+            tabBtn.querySelector("p span").classList.add("hidden");
+        });
+
+        tab_btn.querySelector("p span").classList.remove("hidden");
+        tab_btn.querySelector("p span").classList.add("block");
+
+        features.forEach(feature => {
+            const id = feature.getAttribute("data-feature-id");
+
+            if(trigger_id === id){
+                feature.classList.remove("hidden");
+                feature.classList.add("flex");
+                return;
+            }
+            feature.classList.add("hidden");
+        });
+    }
 
     const toggleAccordionContent = (target) => {
         const paragraph = target.closest("div").querySelector("p");
